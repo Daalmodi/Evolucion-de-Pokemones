3c842abe0be96d22107554358f13bb0cfe6956f8
Daniel Moya <daalmodi@gmail.com>
2024-10-26 18:31:27 -0500

    Evolucion con dm

--- src/EvolutionPokemon.js
+++ src/EvolutionPokemon.js
@@ -3,42 +3,16 @@ import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-hel
 import '@bbva-web-components/bbva-web-panel-outstanding-opportunity/bbva-web-panel-outstanding-opportunity.js';
 import'@bbva-web-components/bbva-core-image/bbva-core-image.js';
 import '@bbva-web-components/bbva-foundations-grid-default-layout';
+import '@pokemondex/pokemon-dm/pokemon-dm.js';
 import styles from './evolution-pokemon.css.js';
 
-/**
- * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
- *
- * This component ...
- *
- * Example:
- *
- * ```html
- *   <evolution-pokemon></evolution-pokemon>
- * ```
- */
 export class EvolutionPokemon extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Description for property
-       */
-      name: {
-        type: String,
-      },
-    };
-  }
+
 
   constructor() {
     super();
     this.pokemoEvolutions = [];
-    this.evolution = [];
-    //this.pokemonId = parseInt(this.getCurrentRoute().params.id);
-    this.pokemonId = 1;// El pokemon id establece  sus evoluciones y es el parametro dado desde la lista de pokemones.
-    this.getSpecies();
-    this.urlEvolutionChain = null;
-    this.datos = [];
-    this.ids = [];
-
+    this.pokemonId =3; 
   }
 
   static get styles() {
@@ -48,67 +22,17 @@ export class EvolutionPokemon extends LitElement {
     ];
   }
 
-  getSpecies() {
-
-      console.log("eNTRO");
-      
-    fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.pokemonId}`)
-      .then((response)=> response.json())
-      .then((data)=>this.getEvolutionChain(data));
-
-  }
-
-  getEvolutionChain(chain) {
-    this.urlEvolutionChain = chain.evolution_chain.url;
 
 
-    fetch(this.urlEvolutionChain)
-      .then((response)=> response.json())
-      .then((data)=>this.pokemonRenders(data));
-
-
-  }
-
-  pokemonRenders(pokemones) {
-
-    const chain = pokemones.chain;
-    const queue = [ chain ];
-
-    while (queue.length > 0) {
-      const species = queue.shift();
-      const urlParts = species.species.url.split('/');
-      const id = parseInt(urlParts[urlParts.length - 2]);
-      this.datos.push({ nombre: species.species.name, url: species.species.url });
-      this.ids.push(id);
-      if (species.evolves_to) {
-        queue.push(...species.evolves_to);
-      }
-    }
-
-    this.getEvolution();
-
-  }
-
-  getEvolution() {
-
-    this.ids.forEach((id) => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then((response) => response.json())
-        .then((data) => this.renderPokemons(data));
-    });
-
-  }
-
-
-  renderPokemons(pokemon) {
-
-    this.pokemoEvolutions = [...this.pokemoEvolutions, pokemon];
+  async firstUpdated(){
+    await this.updateComplete;
+    const evolucion = this.shadowRoot.querySelector('pokemon-dm');
+    await evolucion.fetchPokemonData(this.pokemonId);
+    this.pokemoEvolutions = evolucion.evolutioData;
     this.requestUpdate();
-
   }
 
-
-
+  
   render() {
     return html`
       <bbva-foundations-grid-default-layout layout="[{
@@ -142,10 +66,12 @@ export class EvolutionPokemon extends LitElement {
         </bbva-web-panel-outstanding-opportunity-item>
         
       </div>
- 
+        
       `)}
     </div>
+    
     </bbva-foundations-grid-default-layout>
+    <pokemon-dm></pokemon-dm>
     `;
   }
 }
